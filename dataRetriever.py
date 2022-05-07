dfe1701b45a9bc8a7c4d4f7a3ee8b96fd8495530
irene-poxylari <49757176+irene-poxylari@users.noreply.github.com>
2022-05-08 00:43:47 +0200

    Update dataRetriever.py

--- dataRetriever.py
+++ dataRetriever.py
@@ -3,8 +3,7 @@ import pm4py as pm
 
 
 class DataRetriever():
-
-    """ Class that is used to read data from a file and transforms it into 
+    """ Class that is used to read data from a file and transforms it into
     the necessary format. Some simple analysis is performed to get an impression
     of the data.
     """
@@ -14,47 +13,45 @@ class DataRetriever():
 
         self.df = pd.read_csv("data/" + str(filename), sep=sep, engine='python')
 
-
     def get_dataframe(self):
         """"Returns the dataframe"""
 
         return self.df
-        
+
+    def get_dataHead(self):
+        print( self.df.head())
+
+    def getShape(self):
+        """returns the number of columns and the number of rows"""
+        return self.df.shape
+    def getInfo(self):
+        return self.df.info
+
+    def getDescribe(self):
+        "returns statistical properties for the data set like the mean, count"
+        return self.df.describe()
 
     def create_event_log(self, case_col, act_col, time_col):
         """Creates an event log in the correct format given a pandas dataframe
         TODO: We might want to merge this with the constructor.
         """
-        
-        event_log = pm.format_dataframe(self.df, 
-                                        case_id=case_col, 
+
+        event_log = pm.format_dataframe(self.df,
+                                        case_id=case_col,
                                         activity_key=act_col,
                                         timestamp_key=time_col)
 
         return event_log
 
-
     def create_diagram(self, event_log):
         """Visualizes the data in a model"""
-        
+
         process_tree = pm.discover_process_tree_inductive(event_log)
         bpmn_model = pm.convert_to_bpmn(process_tree)
         pm.view_bpmn(bpmn_model)
 
     def create_overview(self, log):
-
         # print(log.describe())
         print(log.columns)
-        
+
         unique_act = log["Activity"].unique()
-
-
-
-
-retriever = DataRetriever("incidentProcess_custom.csv")
-
-event_log = retriever.create_event_log(case_col='Incident ID', 
-                                       act_col='Activity', 
-                                       time_col='ActivityTimeStamp')
-
-retriever.create_diagram(event_log)
