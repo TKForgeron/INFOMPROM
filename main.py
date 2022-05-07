c51cc7c7c26447788db115bdb44aff55b1d19aef
irene-poxylari <49757176+irene-poxylari@users.noreply.github.com>
2022-05-08 00:44:34 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Press the green button in the gutter to run the script.
+from DataRetriever import DataRetriever
+
+if __name__ == '__main__':
+    retriever = DataRetriever("incidentProcess_custom.csv")
+
+    event_log = retriever.create_event_log(case_col='Incident ID',
+                                           act_col='Activity',
+                                           time_col='ActivityTimeStamp')
+
+    "retriever.create_diagram(event_log)"
+    retriever.get_dataHead()
+
+    print(retriever.getDescribe())
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
